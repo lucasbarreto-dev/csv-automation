3893854b04981c1ffa92fa002e3388cefc6808c5
lucasbarreto92 <lucasbs.code@gmail.com>
2023-08-18 23:29:01 -0300

    Script to write csv files with the extracted and formatted data.

---
+++ writeCsv.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function csvWriter(newFile, index) {
+
+    const headers = [ 'name', 'email', 'telefone' ];
+
+    const csvContent = [headers, ...newFile.map((obj) => headers.map((key) => obj[key]))]
+      .map((row) => row.join(','))
+      .join('\n');
+
+    fs.writeFile(`Base Pessoas - TM-T${index + 1} formatado.csv`, csvContent, (err) => {
+      if (err) {
+        console.error('Error writing CSV file:', err);
+      } else {
+        console.log('CSV file written successfully.');
+      }
+    });
+}
+
+module.exports = { csvWriter };
