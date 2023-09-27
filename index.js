ab744d03c9ab07f56578256509c626b573adda33
lucasbarreto92 <lucasbs.code@gmail.com>
2023-09-27 20:20:49 -0300

    Alterações na arquitetura para escalar a automação.

--- index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { auto } = require('./utils/auto');
-
-const TM = path.resolve(__dirname, './tm/csv');
-const DM = path.resolve(__dirname, './dm/csv');
-
-const directories = [ DM, TM ];
-
-directories.forEach((directory) => auto(directory));
---
+++ utils/google_contacts/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const { auto } = require('./auto');
+
+const TM = path.resolve(__dirname, '../../tm/csv');
+const DM = path.resolve(__dirname, '../../dm/csv');
+
+const directories = [ DM, TM ];
+
+directories.forEach((directory) => auto(directory));
