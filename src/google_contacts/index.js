e173cc34596aa0f2ef64f69408a92b3310511547
lucasbarreto92 <lucasbs.code@gmail.com>
2024-07-01 17:11:23 -0300

    Inclusão dos programas HT e HD

--- src/google_contacts/index.js
+++ src/google_contacts/index.js
@@ -3,7 +3,10 @@ const { auto } = require('./utils/auto');
 
 const TM = path.resolve(__dirname, '../../tm/csv');
 const DM = path.resolve(__dirname, '../../dm/csv');
+const HT = path.resolve(__dirname, '../../ht/csv');
+const HD = path.resolve(__dirname, '../../hd/csv');
 
-const directories = [ DM, TM ];
+
+const directories = [ DM, TM, HT, HD ];
 
 directories.forEach((directory) => auto(directory));
