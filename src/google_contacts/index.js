3eef754abf0559c4cae5e03f0100b1916c838555
Lucas Barreto <88976146+lucasbarreto-dev@users.noreply.github.com>
2025-01-10 00:18:41 -0300

    refactor: cria objeto 'directories' para otimizar o código.

--- src/google_contacts/index.js
+++ src/google_contacts/index.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const { auto } = require('./utils/auto');
 
-const TM = path.resolve(__dirname, '../../tm/csv');
-const DM = path.resolve(__dirname, '../../dm/csv');
-const HT = path.resolve(__dirname, '../../ht/csv');
-const HD = path.resolve(__dirname, '../../hd/csv');
+const directories = {
+  TM: path.resolve(__dirname, '../../tm/csv'),
+  DM: path.resolve(__dirname, '../../dm/csv'),
+  HT: path.resolve(__dirname, '../../ht/csv'),
+  HD: path.resolve(__dirname, '../../hd/csv'),
+  AT: path.resolve(__dirname, '../../at/csv'),
+};
 
-
-const directories = [ DM, TM, HT, HD ];
-
-directories.forEach((directory) => auto(directory));
+Object.values(directories).forEach((directory) => auto(directory));
